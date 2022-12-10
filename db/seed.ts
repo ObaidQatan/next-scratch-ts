c804bacf113c44d84a060952c5edb4cd8b6ef448
Obaid Qatan <ogaten27@gmail.com>
2022-12-11 02:15:21 +0530

    update boilerblate

--- db/seed.ts
+++ db/seed.ts
@@ -1,10 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import { Customer, PrismaClient } from "@prisma/client";
 import encrypt from "../src/util/encrypt";
 const prisma = new PrismaClient();
 
 async function main() {
   // seed here
   // remove all users
+  await prisma.customer.deleteMany({});
+
+  await prisma.customer.createMany({
+    // @ts-ignore
+    data: generateCustomers(),
+  });
 }
 
 main()
@@ -15,4 +21,19 @@ main()
     await prisma.$disconnect();
   });
 
+const generateCustomers = () => {
+  const customers = [];
+  for (let i = 0; i < 10; i++) {
+    customers.push({
+      email: `email${i}@email.com`,
+      password: encrypt("123456"),
+      name: `Customer${i}`,
+      balance: parseFloat((Math.random() * 4000 + 1000).toFixed(2)),
+      role: "USER",
+      username: `cus${i}`,
+    });
+  }
+
+  return customers;
+};
 export {};
